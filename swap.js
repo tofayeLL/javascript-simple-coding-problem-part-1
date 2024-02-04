944e5b4409f26d9359da341ee7a2905226fb9863
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-04 12:26:14 +0600

    practice and learn about swap by use temp and distructing

---
+++ swap.js
@@ -0,0 +1,25 @@
+
+
+// swap value by using temp
+
+let a = 7;
+let b = 5;
+let temp = 0;
+console.log(a, b);
+ temp = a;
+ a = b;
+ b = temp;
+ console.log(a, b);
+// -------------------------------------//
+
+
+
+// by using destructing 
+let x = 5;
+let y = 7;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x , y);
+
+//--------------------------------------//
+ 

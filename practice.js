572b3a55e2df8c0e2793cb6bb3bf0a7d9cc55321
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-04 16:42:50 +0600

    finished practice task three about check vowel in string and count vowel

--- practice.js
+++ practice.js
@@ -17,7 +17,7 @@ console.log(convert); */
 /* ---practice task two--- */
 /* question: You are given an array of numbers. Count how many times the a number is repeated in the array. */
 
-function countDuplicate(numbers){
+/* function countDuplicate(numbers){
   let countDuplicate = 0;
   let search = [6];
   for(let num of numbers){
@@ -31,4 +31,35 @@ function countDuplicate(numbers){
 
 let numbs = [5,6,11,12,98, 5, 6, 6, 6];
 let result = countDuplicate(numbs);
-console.log('Duplicate Numbers:', result);
+console.log('Duplicate Numbers:', result); */
+
+
+
+
+
+/* ------practice task three--- */
+/* question: Write a function to count the number of vowels in a string. */
+
+function  countVowels(str){
+  let count = 0;
+  let vowels = 'aeiouAEIOU';
+  for(let i = 0; i < str.length; i++){
+    let characters = str[i];
+    if(vowels.includes(characters)){
+      count ++;
+    }
+  }
+  return count;
+  
+}
+
+let sentence = 'i am a Serious Learner';
+let vowels = countVowels(sentence);
+console.log('Total vowels in the string or sentences :', vowels);
+
+
+
+
+/* ---practice task five----- */
+/* question: Generate a random number between 10 to 20 */
+
